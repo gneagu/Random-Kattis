8fb569fe184020b2238f986da94eb71c6977a482
gneag <neagu@ualberta.ca>
2018-05-22 11:25:17 -0600

    derp

--- parsinghex.py
+++ parsinghex.py
@@ -3,3 +3,14 @@
 # Gregory Neagu
 # May 21, 2018
 ###########################################################
+import sys
+
+totalInput = ""
+totalInput = sys.stdin.readlines()
+
+for line in totalInput:
+    x = length(totalInput)
+    i = 0
+
+    while i < x:
+        if line[i] == "0":
